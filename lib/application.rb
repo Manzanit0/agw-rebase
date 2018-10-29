1c3160f99e142b172cfc5f8f3785593083517f49
Manzanit0 <unico.javier@gmail.com>
2018-10-29 11:57:42 +0000

    Refactors run script to simply run the game. Also cleans application.rb.

--- lib/application.rb
+++
@@ -1,8 +0,0 @@
-# Loads 'lib/' to the $LOAD_PATH
-libdir = File.dirname(__FILE__)
-$LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
-
-require "console/console"
-
-console = Console.new
-console.play
--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
-if [ ! -f $HOME/bin/bundle ]; then
-    gem install bundler --no-document
-fi
+#!/usr/bin/ruby -w
 
-echo "Installing Tictactoe dependencias via Bundler..."
-bundle install --quiet
+# Loads 'lib/' to the $LOAD_PATH
+libdir = File.dirname(__FILE__) + '/lib/'
+puts libdir
+$LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 
-echo "Executing app..."
-ruby lib/application.rb
+require "console/console"
+
+console = Console.new
+console.play
